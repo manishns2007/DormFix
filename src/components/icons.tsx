49275b43c560982d89c8fc9c6c3624727315c4c9
NARESH KUMAR N IT <nareshkumarn.it2024@citchennai.net>
2025-08-18 10:48:59 +0000

    Update the Category dropdown menu by adding a new option “Water Dispense

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,13 +1,15 @@
-import { Armchair, Snowflake, Wrench, Zap, LucideProps, Construction } from 'lucide-react';
+import { Armchair, Snowflake, Wrench, Zap, LucideProps, Construction, GlassWater } from 'lucide-react';
 import { MaintenanceCategory } from '@/lib/types';
 
 export const Icons = {
   logo: Wrench,
-  furniture: Armchair,
+  'furniture and door': Armchair,
   ac: Snowflake,
   plumbing: Wrench,
   electrical: Zap,
   lift: Construction,
+  'water dispenser': GlassWater,
+  water: Wrench,
 };
 
 export function CategoryIcon({ category, ...props }: { category: MaintenanceCategory } & LucideProps) {
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const categories = ["AC", "Plumbing", "Electrical", "Furniture", "Lift", "Water"] as const;
+export const categories = ["AC", "Plumbing", "Electrical", "Furniture and Door", "Lift", "Water", "Water Dispenser"] as const;
 export type MaintenanceCategory = (typeof categories)[number];
 
 export const priorities = ["Low", "Medium", "High"] as const;
